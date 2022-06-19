401e1271161c188c0cb096571590308c4c02c245
LiaoliaoLiu <liu.liaoliao@outlook.com>
2022-06-18 20:31:59 -0700

    make helper functions nested

--- 15.3-sum.ts
+++ 15.3-sum.ts
@@ -5,36 +5,29 @@
  */
 
 // @lc code=start
-function twoSum(nums: number[], i: number, results: number[][]): number[][] {
-    let left = i+1;
-    let right = nums.length - 1;
-    while (left < right) {
-
-        const sum = nums[left] + nums[right] + nums[i];
-        if (sum < 0) {
-            left += 1;
-        } else if (sum > 0) {
-            right -= 1;
-        } else {
-            results.push([nums[i], nums[left], nums[right]]);
-            left += 1;
-            right -= 1;
-            while (left < right && nums[left] === nums[left-1]) left += 1;
-        }
-
-        // -3 1 1 1 1 1 2
-    }
-    return results;
-}
 function threeSum(nums: number[]): number[][] {
-    nums.sort((a, b) => a - b);
-    const results: number[][] = [];
-    for (let i = 0; i < nums.length; i += 1) {
-        if (i > 0 && nums[i] === nums[i-1]) {
-            continue;
+    function twoSum(nums: number[], i: number, results: number[][]) {
+        let left = i+1, right = nums.length-1;
+        while (left < right) {
+            let sum = nums[i] + nums[left] + nums[right];
+            if (sum < ) left += 1;
+            else if (sum > 0) right -= 1;
+            else {
+                results.push([nums[i], nums[left], nums[right]]);
+                left += 1;
+                right -= 1;
+                while (nums[left] === nums[left-1]) left += 1;
+            }
         }
+    }
+    nums.sort((a, b) => a-b);
+    let results: number[][] = [];
+    for (let i = 0; i < nums.length; i += 1) {
+        // fix nums[i] and do a two sum
+        while (i > 0 && nums[i] === nums[i-1]) i += 1;
         twoSum(nums, i, results);
     }
+
     return results;
 };
 // @lc code=end
--- 18.4-sum.ts
+++ 18.4-sum.ts
@@ -6,36 +6,29 @@
 
 
 // @lc code=start
-function twoSum(nums: number[], i: number, j: number, target: number, results: number[][]) {
-    let left = j+1, right = nums.length-1;
-    while (left < right) {
-        let sum = nums[i] + nums[j] + nums[left] + nums[right];
-        if (sum < target) left += 1;
-        else if (sum > target) right -= 1;
-        else {
-            results.push([nums[i], nums[j], nums[left], nums[right]]);
-            left += 1;
-            right -= 1;
-            // [-1 -1 1 1 2 2 2 2] 1 
-            //   i  j     l   r
-            while (nums[left] === nums[left-1]) left += 1;
+function fourSum(nums: number[], target: number): number[][] {
+    function twoSum(nums: number[], i,j,target:number, results: number[][]) {
+        let left = j+1, right = nums.length - 1;
+        while (left < right) {
+            let sum = nums[i] + nums[j] + nums[left] + nums[right];
+            if (sum < target) left += 1;
+            else if (sum > target) right -= 1;
+            else {
+                results.push([nums[i], nums[j], nums[left], nums[right]]);
+                left += 1;
+                right -= 1;
+                while (nums[left] === nums[left-1]) left += 1;
+            }
         }
     }
-}
-function fourSum(nums: number[], target: number): number[][] {
-    const results: number[][] = [];
-    nums.sort((a, b) => a - b);
+
+    nums.sort((a,b) => a-b);
+    let results: number[][] = [];
     for (let i = 0; i < nums.length; i += 1) {
-        // [-1 -1 ...]
-        if (i > 0 && nums[i] === nums[i-1]) {
-            continue;
-        }
+        while (i > 0 && nums[i] === nums[i-1]) i += 1;
         for (let j = i+1; j < nums.length; j += 1) {
-            if (j > i+1 && nums[j] === nums[j-1]) {
-                continue;
-            }
-            //two sum
-            twoSum(nums, i, j, target, results)
+            while (j > i+1 && nums[j] === nums[j-1]) j += 1;
+            twoSum(nums, i, j, target, results) 
         }
     }
 
